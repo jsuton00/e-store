663c2812b604ceb1724eea73ed5d75e9465170b8
Jozo Suton <61884667+jsuton00@users.noreply.github.com>
2021-06-16 19:07:24 +0200

    removed any problems o errors

--- src/components/Cart/PurchaseControl.jsx
+++ src/components/Cart/PurchaseControl.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import AddtoCart from './AddtoCart';
 import { connect } from 'react-redux';
 import * as actions from '../../store/actions/index';
